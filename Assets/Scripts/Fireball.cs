82d7416835bb2a6a091a18e1b950d22005875e05
rextragalactic <95050023+rextragalactic@users.noreply.github.com>
2022-03-23 01:05:17 +0100

    Working on the Shooting Process

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -9,14 +9,14 @@ public class Fireball : MonoBehaviour
     public float lifeTime;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         //Destroy Fireball after Life Time has passed
-        DestroyObject(gameObject, lifeTime);
+        Destroy(gameObject, lifeTime);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
     }
